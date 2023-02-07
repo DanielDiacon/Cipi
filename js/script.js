bcf9c3ad688097ee7177696791a5de3fe6980604
Daniel <110478467+DanielDiacon@users.noreply.github.com>
2023-02-07 18:34:05 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,35 +1,34 @@
 // menu burger <=>
-//let body = document.querySelector('body');
-//const burger = document.getElementById('burger');
-//const menu = document.getElementById('menu');
-//const logo = document.getElementById('logo');
-//logo.onclick = function () {
-//   logo.classList.toggle('active');
-//};
-//burger.onclick = function () {
-//   burger.classList.toggle('active');
-//   menu.classList.toggle('active');
-//};
-//document.onclick = function (e) {
-//   if (e.target.id !== 'burger' && e.target.classList !== 'menu' && e.target.id !== 'logo') {
-//      burger.classList.remove('active');
-//      menu.classList.remove('active');
-//   }
-//};
+let body = document.querySelector('body');
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+
+burger.onclick = function () {
+   burger.classList.toggle('active');
+   menu.classList.toggle('active');
+   body.classList.toggle('lock');
+};
+document.onclick = function (e) {
+   if (e.target.id !== 'burger' && e.target.classList !== 'menu' && e.target.id !== 'logo') {
+      burger.classList.remove('active');
+      menu.classList.remove('active');
+      body.classList.remove('lock');
+   }
+};
 
 
 // header show <=>
-//var lastScrollTop = 0;
-//   navbar = document.getElementById('navbar');
-//window.addEventListener("scroll" , function(){
-//   var scrollTop = window.pageXOffset || document.documentElement.scrollTop;
-//   if (scrollTop > lastScrollTop){
-//      navbar.style.top='-50px'
-//   } else {
-//      navbar.style.top='0'
-//   }
-//   lastScrollTop = scrollTop;
-//});
+var lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+window.addEventListener("scroll", function () {
+   var scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+   if (scrollTop > lastScrollTop) {
+      navbar.style.top = '-80px'
+   } else {
+      navbar.style.top = '0'
+   }
+   lastScrollTop = scrollTop;
+});
 
 
 //  animation for scroll <=>
