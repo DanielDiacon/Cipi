88880686544dece63b3982de018a6584bc76c276
Daniel <110478467+DanielDiacon@users.noreply.github.com>
2023-02-10 16:34:26 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,18 +18,23 @@ document.onclick = function (e) {
 
 
 // header show <=>
-var lastScrollTop = 0;
-navbar = document.getElementById('navbar');
-window.addEventListener("scroll", function () {
-   var scrollTop = window.pageXOffset || document.documentElement.scrollTop;
-   if (scrollTop > lastScrollTop) {
-      navbar.style.top = '-80px'
-   } else {
-      navbar.style.top = '0'
-   }
-   lastScrollTop = scrollTop;
-});
+//var lastScrollTop = 0;
+//   navbar = document.getElementById('navbar');
+//window.addEventListener("scroll" , function(){
+//   var scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+//   if (scrollTop > lastScrollTop){
+//      navbar.style.top='-50px'
+//   } else {
+//      navbar.style.top='0'
+//   }
+//   lastScrollTop = scrollTop;
+//});
 
+
+//  animation for scroll <=>
+//ScrollOut({
+//   targets: '.title , .text , .block'
+//});
 
 
 // loading page <=>
