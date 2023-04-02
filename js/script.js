afd09474cd885bfd7ee177eba9b16904090998e4
Daniel <110478467+DanielDiacon@users.noreply.github.com>
2023-04-02 22:30:36 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// dropdown portofolio
+let submenu = document.getElementById('submenu');
+let dropdown = document.getElementById('dropdown');
+let dropdownMenu = document.getElementById('dropdownMenu');
+dropdown.onclick = function () {
+   dropdownMenu.classList.toggle('active');
+   dropdown.classList.toggle('active');
+   submenu.classList.toggle('active');
+
+};
+
 // menu burger <=>
 let body = document.querySelector('body');
 const burger = document.getElementById('burger');
@@ -7,12 +18,24 @@ burger.onclick = function () {
    burger.classList.toggle('active');
    menu.classList.toggle('active');
    body.classList.toggle('lock');
+   dropdownMenu.classList.remove('active');
+   dropdown.classList.remove('active');
+   submenu.classList.remove('active');
 };
 document.onclick = function (e) {
-   if (e.target.id !== 'burger' && e.target.classList !== 'menu' && e.target.id !== 'logo') {
+   if (e.target.id !== 'burger' 
+   && e.target.classList !== 'menu' 
+   && e.target.id !== 'logo'
+   && e.target.id !== 'submenu'
+   && e.target.id !== 'dropdown'
+   && e.target.id !== 'dropdownMenu'
+   ) {
       burger.classList.remove('active');
       menu.classList.remove('active');
       body.classList.remove('lock');
+      dropdownMenu.classList.remove('active');
+      dropdown.classList.remove('active');
+      submenu.classList.remove('active');
    }
 };
 
@@ -23,20 +46,51 @@ window.addEventListener("scroll", function () {
    var scrollTop = window.pageXOffset || document.documentElement.scrollTop;
    if (scrollTop > lastScrollTop) {
       navbar.style.top = '-80px'
+      dropdownMenu.classList.remove('active');
+      dropdown.classList.remove('active');
+      submenu.classList.remove('active');
    } else {
       navbar.style.top = '0'
    }
    lastScrollTop = scrollTop;
 });
 
+
 // loading page <=>
 const loader = document.getElementById("loader");
 window.addEventListener("load", function () {
-   loader.style.display = "none";
+   loader.style.visibility = "hidden";
+   loader.style.opacity = "0";
+   loader.style.transition = "0.7s";
 });
 
-// swiper slider
+// popup <=>
+// Open popup
+document.querySelector(".btn").addEventListener("click", function () {
+   document.querySelector(".popup").classList.add("active");
+   document.documentElement.classList.add("hidden");
+});
 
+// Close popup when clicking on button
+document.querySelector(".close").addEventListener("click", function () {
+   document.querySelector(".popup").classList.remove("active");
+   document.documentElement.classList.remove("hidden");
+});
+
+// Close menu when clicking outside the element area
+document.addEventListener("click", (e) => {
+   const withinBoundaries =
+      e.composedPath().includes(document.querySelector(".inner")) ||
+      e.composedPath().includes(document.querySelector(".btn")) ||
+      e.composedPath().includes(document.querySelector(".close"));
+
+   if (!withinBoundaries) {
+      document.querySelector(".popup").classList.remove("active");
+   }
+});
+
+
+// swiper slider
 new Swiper('.swiper-container', {
    pagination: {
       el: '.swiper-pagination',
@@ -54,3 +108,5 @@ new Swiper('.swiper-container', {
       el: '.swiper-scrollbar',
    },
 });
+
+
